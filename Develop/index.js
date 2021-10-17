17e4d7d7ea7c94ed9a3980a02b42f8badcd5b6a5
DesmondDeon <youngdesmondd7@gmail.com>
2021-10-17 16:25:07 -0400

    git ignore active

---
+++ Develop/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const markdown = require("");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {   
+        message: "What is the name of the project?",
+        name: "title"
+    },
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
