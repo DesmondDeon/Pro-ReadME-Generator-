90ed6c598c66a274f7d40dd81558c2a65e75a91f
DesmondDeon <youngdesmondd7@gmail.com>
2021-10-17 23:09:46 -0400

    code base

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,91 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const axios = require("axios");
+const axios = require('axios');
 const inquirer = require("inquirer");
-const markdown = require("");
+const markdown = require("./utils/generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {   
-        message: "What is the name of the project?",
-        name: "title"
-    },
-
-];
+        {
+            type: 'input',
+            message: 'What is the name of the Repository?',
+            name: 'name',
+            validate: (value) => {if(value){return true}else{return 'Please Name This Repository'}},
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this Repository?',
+            description: 'description',
+            validate: (value) => {if(value){return true}else{return 'Please Describe This Project'}},
+        },
+        {
+            type: 'input',
+            message: 'What is your Git Username?',
+            name: 'github username',
+            validate: (value) => {if(value){return true}else{return 'I need this value to continue '}},
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+            validate: (value) => {if(value){return true}else{return 'I need this value to continue '}},
+        },
+        {
+            type: 'input',
+            message: 'Who contributed to this repository?',
+            name: 'contributers',
+            validate: (value) => {if(value){return true}else{return 'Desmond Young'}},
+        },
+        {
+            type: 'input',
+            message: 'Which Respositories are simular? and Why?',
+            name: 'works',
+            validate: (value) => {if(value){return true}else{return 'Please list names of Repositories'}},
+        },
+        {
+            type: 'checkbox',
+            message: 'Which Licenses did you use? ',
+            name: 'licenses',
+            choices: ['MIT','GPLv3','Apache 2.0', 'GPL'],
+            validate: (value) => {if(value){return true}else{return 'This value is needed to continue'}},
+        }
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const createNewFile = ReadMeFile => {
+    return new Promise ((worked, error) => {
+            fs.writeFile(`./${filename.toLowercase().split(' ').join('')}.md`,data,()=>{
+                if(err){
+                    // console.log(err)
+                    error(err);
+                    return;
+                }
+
+            worked({
+                ok: true,
+                message: 'Your ReadME is Ready!'
+            });
+        });
+    },
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {
+        console.log("Making ReadMe");
+        fs.writeFileSync("ReadMe.md", inquirerResponse, data);
+    })
+    .then(ReadMeFile => {
+        return generateMarkdown(ReadMeFile)
+    })
+    .then(ReadMeFile => {
+        return createNewFile(ReadMeFile)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license.license; // i think this is problem #1
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +26,55 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  [![Repo Issues](https://img.shields.io/bitbucket/issues/DesmondDeon/Pro-ReadME-Generator-.svg)]()
+  [![Forks](https://img.shields.io/github/forks/DesmondDeon/Pro-ReadME-Generator- .svg)]()
+  [![Language](https://img.shields.io/github/languages/top/DesmondDeon/Pro-ReadME-Generator-.svg)]()
 
-`;
+  
+  # Table of Content
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[Licenses](#licenses)
+  *[Contributions](#contribution)
+  *[Work](#work)
+  *[Username](#username)
+  *[Profile](#profile)
+
+  ${github}
+  ##username:
+
+      ${description}
+  ##description:
+  
+      ${data.installation}
+  ##installation:
+  
+  ##License
+      ${renderLicenseBadge(license)}
+  
+      ${contributions}
+  ##contributions:
+  Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+  Please make sure to update tests as appropriate.
+
+      ${works}
+  ##works:
+
+  ##email:
+  [${email}](mailto:${email})
+  
+      ${profile}
+  ##profile:
+  
+  #Contacts
+  * Github :${github}
+  * Linkedin :${linkedin}
+  * Email :${email}
+  
+Copyright (c) Desmond Young SP. All rights reserved.`; 
 }
+
+console.log(generateMarkdown)
 
 module.exports = generateMarkdown;
